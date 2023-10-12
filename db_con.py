1dbaf02c3e0d658ff55b53f219735cf1a7b7024d
KB0129 <dlrudqls99@gmail.com>
2023-10-11 17:46:01 -0700

    make create_db table()

--- db_con.py
+++ db_con.py
@@ -8,7 +8,31 @@ import sqlite3
 def get_db():
     #Postgres
     #return psycopg2.connect(host="localhost", dbname="authme" , user="loki", password="4prez")
-    return sqlite3.connect("local_data_base")
+    return sqlite3.connect("database.db")
+
+def create_db_table():
+    try:
+        conn = get_db()
+        conn.execute('''
+            CREATE TABLE users (
+                user_id INTEGER PRIMARY KEY NOT NULL,
+                name TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL,
+                address TEXT NOT NULL,
+                phone TEXT NOT NULL,
+                gender TEXT NOT NULL,
+                birth DATA NOT NULL, 
+                introduce TEXT NOT NULL 
+            );
+        ''')
+        conn.commit()
+        print("User table is created successfully!")
+    except:
+        print("Failed to create table")
+    finally:
+        conn.close()
+
 
 def get_db_instance():  
     db  = get_db()
