880bc2643104c2ffa60f4ec5d57d49e23207c203
ehecha <124849863+hecha002csusmedu@users.noreply.github.com>
2023-10-16 14:48:14 -0700

    Add files via upload

---
+++ open_calls/stop_movie.py
@@ -0,0 +1,10 @@
+from flask import request, g                                                                 
+from tools.logging import logger   
+from neurosdk.cmn_types import * 
+
+def handle_request():
+    if g.hb == None:
+        return ["Data Flowing"]
+
+    g.hb.exec_command(SensorCommand.CommandStopSignal)
+    return ["Data Flowing"]
