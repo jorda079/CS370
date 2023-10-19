fea5ccf21f495eb11303af43a12f97370c93d7bc
KB0129 <dlrudqls99@gmail.com>
2023-10-18 21:18:51 -0700

    remove action=.php in html

--- auth.py
+++ auth.py
@@ -6,7 +6,8 @@ auth = Blueprint('auth', __name__, static_folder='static', template_folder='temp
 
 # sign up for backend
 @auth.route("/signup", methods=['GET', 'POST'])
-def signup():
+def registration():
+    db, cur = get_db_instance()
     # user is not currently logged in
     if 'loggged_in' not in session:
         # data will be available from POST submitted by the form
@@ -17,12 +18,19 @@ def signup():
             password = request.form['password']
             # password2 = request.form['password2']
 
-            db, cur = get_db_instance()
-            cur.execute("INSERT INTO users (email, name, password) VALUES (?, ?, ?,)", (email, username, password))
-            db.commit()
-            return redirect(url_for('index.html'))
+            # check both email & password already exist
+            cur.execute(f"SELECT * from users WHERE email='{email}' AND password=''{password};")
+            data = cur.fetchone()
+            if data:
+                return render_template("error.html")  # duplicated email & password
+            else:
+                if not data:
+                    cur.execute(cur.execute("INSERT INTO users (email, name, password) VALUES (?, ?, ?,)", (email, username, password)))
+                    db.commit()
+                    db.close()
+                return redirect(url_for('login.html'))
 
-    # user is alredy logged in
+    # user is already logged in
     else:
         print("You are alredy logged in.")
     return render_template("index.html")
