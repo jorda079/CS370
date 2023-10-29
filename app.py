0b452c66981a0c35eb81b46b99f7ebffce30f98d
KB0129 <dlrudqls99@gmail.com>
2023-10-28 22:22:50 -0700

    done signup, update login

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import bcrypt
 import traceback
 
 # ! temporarily except egg.py to avoid error
-from tools.eeg import get_head_band_sensor_object
+# from tools.eeg import get_head_band_sensor_object
 
 
 from db_con import get_db_instance, get_db
@@ -64,7 +64,7 @@ def index():
 
 @app.route('/main')
 def main():
-    return render_template('main.html')
+    return render_template('mainpage.html')
 
 @app.route('/inbox')
 def inbox():
